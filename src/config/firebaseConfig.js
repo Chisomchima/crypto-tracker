7736e4d5cfc6d51dd0543d43f0fc41a25175c44a
chisom chima <chimachisom360@gmail.com>
2023-11-08 17:25:49 +0100

    complete firebase authentication

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdTxXb9BG83T5S46iErVKCGtWmVseqbPA",
+  authDomain: "crypto-app-411c1.firebaseapp.com",
+  projectId: "crypto-app-411c1",
+  storageBucket: "crypto-app-411c1.appspot.com",
+  messagingSenderId: "843072714734",
+  appId: "1:843072714734:web:109af21f48b0dd607f00ff",
+  measurementId: "G-Z284BHLC86"
+};
+
+export default firebaseConfig;
