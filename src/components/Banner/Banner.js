fdedfccb9b06d76c90f8c5f02c5fa34ce32d8ed1
chisom chima <chimachisom360@gmail.com>
2023-11-10 08:37:45 +0100

    sdbfhrbhj

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            CryptoTracker
           </Typography>
           <Typography
             variant="subtitle2"
--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto Hunter
+              CryptoTracker
             </Typography>
             <Select
               variant="outlined"
